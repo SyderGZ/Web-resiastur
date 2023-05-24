d720f9400e7abbf542758dbca0cf48a60a1c1929
SyderGZ <ericgalindo94@gmail.com>
2023-05-24 20:47:01 +0200

    A

--- sesion/iniciarsesion.php
+++ sesion/iniciarsesion.php
@@ -1,25 +1,43 @@
 <?php include '../includes/config.php';    ?>
 <?php include '../includes/_header.php';    ?>
 
+    <div class="container">
+	<div class="screen">
+		<div class="screen__content">
+			<form action="controlador-login.php" method="post" class="login">
 
-<h1>Inicio de sesión</h1>
-    
-<form action="controlador-login.php" method="post">
-    <div class="iniciarsesion">
-    <i class="fa-solid fa-user"></i>
-        <input name="username" type="text" placeholder="Escribe tu nombre de usuario">
-    </div>
-                <br><br>
-    <div class="iniciarsesion">
-    <i class="fa-solid fa-lock"></i>
-        <input name="password" type="password" placeholder="Contraseña" >
-    </div>
-                <br><br>
-        <input id="submit" type="submit" value="Iniciar sesión" name="btniniciar">
-        <button><a href="http://resiastur.local/registrar.php">Registrarse</a></button>
-        </form>
-   
 
+				<div class="login__field">
+					<i class="login__icon fas fa-user"></i>
+					<input name="username" type="text" class="login__input" placeholder="Nombre de Usuario">
+				</div>
+
+
+				<div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+					<input name="password" type="password" class="login__input" placeholder="Contraseña">
+				</div>
+				
+
+                <div class="button login__submit">
+                <input id="submit" type="submit" value="Iniciar sesión" name="btniniciar">
+					<i class="button__icon fas fa-chevron-right"></i>
+                </div>
+				
+				<button class="button login__submit">
+                    <a href="http://resiastur.local/registrar.php" class="button__text">Registrarse</a>
+					<i class="button__icon fas fa-chevron-right"></i>
+				</button>
+			</form>
+		</div>
+		<div class="screen__background">
+			<span class="screen__background__shape screen__background__shape4"></span>
+			<span class="screen__background__shape screen__background__shape3"></span>		
+			<span class="screen__background__shape screen__background__shape2"></span>
+			<span class="screen__background__shape screen__background__shape1"></span>
+		</div>		
+	</div>
+</div>
         
     <a href="../index.php">Volver al inicio</a>
 
