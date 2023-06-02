9b13ca3b0cb5d576a1d911ece3cb10085ae79abe
SyderGZ <ericgalindo94@gmail.com>
2023-06-02 21:21:14 +0200

    ss
    sad

---
+++ error/404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="http://resiastur.local/error/style.css">
+</head>
+<body>
+  
+<section class="page_404">
+	<div class="container2">
+		<div class="row">	
+		<div class="col-sm-12 ">
+		<div class="col-sm-10 col-sm-offset-1  text-center">
+		<div class="four_zero_four_bg">
+		</div>
+		
+		<div class="contant_box_404">
+		<h3 class="h2">
+		Estás un poco perdido por aquí
+		</h3>
+		
+		<p>La página que estás buscando no esta disponible!</p>
+		
+		<a href="http://resiastur.local/inicio" class="link_404">Volver al Inicio</a>
+	</div>
+		</div>
+		</div>
+		</div>
+	</div>
+</section>
+
+
+
+
+<div class="container">
+  <div class="canvas">
+  <canvas id="space-invaders"/>
+  </div>
+</canvas>
+<div class="text">
+  <h1>Puedes entretenerte un rato jugando</h1>
+<button class="btn btn-default btn-xs" id="restart">Empieza el juego</button>
+<p class="center">Utiliza la barra espaciadora para disparar.
+  <br/> Y las flechitas para moverte
+  </p>
+</div>
+</div>
+
+<script src="http://resiastur.local/error/script.js"></script>
+</body>
+</html>
